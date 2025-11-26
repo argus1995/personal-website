a8284d402d41db5f258860ec9d2db17e7a914e7b
Arief Agus Ainur Maulidz <116246237+argus1995@users.noreply.github.com>
2025-11-26 15:31:37 +0700

    Revise portfolio projects and add new entries
    Updated project entries with new images and added two in-progress projects.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,46 +1,56 @@
 const projects = [
   {
     id: 1,
-    title: "Todo App",
-    description: "Simple Todo App with HTML, CSS and JavaCript",
-    tags: ["HTML", "CSS", "JavaScript"],
-    link: "https://simple-todoist-clone.vercel.app/",
-    image: "/portfolio-1.png",
-  },
-  {
-    id: 2,
     title: "Education Website",
     description: "Responsive Education Website using React and CSS",
     tags: ["React", "CSS"],
     link: "https://advance-fe-2.vercel.app/",
-    image: "/portfolio-2.png",
+    image: "/education-website.png",
   },
   {
-    id: 3,
+    id: 2,
     title: "Portfolio Website",
     description:
       "My own personal portfolio website using React and TailwindCSS",
     tags: ["React", "TailwindCSS"],
     link: "https://ariefagus.vercel.app/",
-    image: "/portfolio-3.png",
+    image: "/portfolio-website.png",
   },
   {
-    id: 4,
+    id: 3,
     title: "KPI Tracking App",
     description:
       "A web-based system for managing and reporting organizational performance metrics.",
     tags: ["React", "TailwindCSS, Axios"],
     link: "https://github.com/Rezeon/vpbi-kpi-tracker-frontend",
-    image: "/portfolio-4.png",
+    image: "/kpi-tracking-app.png",
   },
   {
-    id: 5,
+    id: 4,
     title: "Indonesian Resto",
     description:
       "A simple and modern Indonesian restaurant web app built using React, Vite, HTML, and CSS",
     tags: ["html", "CSS", "React", "Vite"],
     link: "https://indonesian-resto.vercel.app/",
-    image: "/portfolio-5.png",
+    image: "/indonesian-resto.png",
+  },
+   {
+    id: 5,
+    title: "CMMS App (in progress)",
+    description:
+      "Backend service for a Computerized Maintenance Management System (CMMS) built using Express.js, Prisma ORM, and Neon PostgreSQL.",
+    tags: ["nodejs", "jwt", "expressjs", "prisma"],
+    link: "https://github.com/argus1995/cmms-app",
+    image: "/cmms-app.png",
+  },
+  {
+    id: 6,
+    title: "Ticketing Api (in progress)",
+    description:
+      "A simple Ticketing System REST API built with Laravel",
+    tags: ["laravel", "rest-api", "sanctum", "mysql"],
+    link: "https://github.com/argus1995/ticketing-api",
+    image: "/ticketing-api.png",
   },
 ];
 
